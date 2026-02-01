b5b3a1eaab55603d7fad827ac6932d00da5eae60
Blu3Souls <Blu3SoulsIT@gmail.com>
2026-02-01 15:45:09 +0100

    fixed openssl build failure

--- flake.nix
+++ flake.nix
@@ -3,6 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -21,11 +24,16 @@
       {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
-          config = {
-            permittedInsecurePackages = [
-              "openssl-1.1.1w"
-            ];
-          };
+          overlays = [
+            (final: prev: {
+              openssl_1_1 = (import inputs.nixpkgs-stable {
+                inherit system;
+                config.permittedInsecurePackages = [
+                  "openssl-1.1.1w"
+                ];
+              }).openssl_1_1;
+            })
+          ];
         };
 
         packages.default = pkgs.callPackage ./packages/x1-control-panel { inherit name; };
